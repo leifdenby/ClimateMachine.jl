f200060cd963c237454b896308b0b202b1dc7d48
K Pamnany <kpamnany@gmail.com>
2020-06-15 08:16:24 -0700

    Diagnostics group for perturbations from horizontal averages
    For LES configurations. Requires that an `InterpolationConfiguration`
    be provided. Added to the surface bubble experiment for testing.

--- src/Diagnostics/groups.jl
+++ src/Diagnostics/groups.jl
@@ -152,6 +152,43 @@ function setup_atmos_core_diagnostics(
     )
 end
 
+include("atmos_les_default_perturbations.jl")
+"""
+    setup_atmos_default_perturbations(
+        ::AtmosLESConfigType,
+        interval::Int,
+        out_prefix::String;
+        writer::AbstractWriter,
+        interpol = nothing,
+    )
+
+Create and return a `DiagnosticsGroup` containing the
+"AtmosLESDefaultPerturbations" diagnostics for the LES configuration.
+All the diagnostics in the group will run at the specified `interval`,
+and written to files prefixed by `out_prefix` using `writer`.
+"""
+function setup_atmos_default_perturbations(
+    ::AtmosLESConfigType,
+    interval::String,
+    out_prefix::String;
+    writer = NetCDFWriter(),
+    interpol = nothing,
+)
+    # TODO: remove this
+    @assert !isnothing(interpol)
+
+    return DiagnosticsGroup(
+        "AtmosLESDefaultPerturbations",
+        Diagnostics.atmos_les_default_perturbations_init,
+        Diagnostics.atmos_les_default_perturbations_fini,
+        Diagnostics.atmos_les_default_perturbations_collect,
+        interval,
+        out_prefix,
+        writer,
+        interpol,
+    )
+end
+
 include("atmos_refstate_perturbations.jl")
 """
     setup_atmos_refstate_perturbations(
