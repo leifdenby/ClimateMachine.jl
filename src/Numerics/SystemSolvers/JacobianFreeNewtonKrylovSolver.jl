74fd3d8969cc050b4c8e82d748c3504d7c9b90a2
Thomas Gibson <gibsonthomas1120@hotmail.com>
2020-07-29 11:17:20 -0700

    Sketch out Jacobian-free Newton iteration

--- src/Numerics/SystemSolvers/JacobianFreeNewtonKrylovSolver.jl
+++ src/Numerics/SystemSolvers/JacobianFreeNewtonKrylovSolver.jl
@@ -1,23 +1,28 @@
 
 export BatchedJacobianFreeNewtonKrylovSolver
 
-mutable struct BatchedJacobianFreeNewtonKrylovSolver{ET} <: AbstractNonlinearSolver
+mutable struct BatchedJacobianFreeNewtonKrylovSolver{ET, AT} <: AbstractNonlinearSolver
     # Tolerance
     ϵ::ET
     # Max number of Newton iterations
     M::Int
     # Maximum number of batched Newton methods (number of vertical columns)
     batch_size::Int
+    jvp!
     # Linear solver for the Jacobian system
-    linearsolver!
+    linearsolver
+    residual::AT
     function BatchedJacobianFreeNewtonKrylovSolver(
         Q::AT,
-        linearsolver!,
+        jvp!,
+        linearsolver,
         batch_size::Int;
         ϵ = √eps(eltype(AT)),
         M = min(20, eltype(Q)),
     ) where {AT}
-        return new{typeof(ϵ)}(ϵ, M, batch_size, linearsolver!)
+        residual = similar(Q)
+        ET = typeof(ϵ)
+        return new{ET, AT}(ϵ, M, batch_size, jvp!, linearsolver, residual)
     end
 end
 
@@ -31,13 +36,44 @@ function initialize!(
     return false, zero(eltype(Q))
 end
 
-function nonlineariteration!(
+function donewtoniteration!(
     implicitoperator!,
     Q,
     Qrhs,
     solver::BatchedJacobianFreeNewtonKrylovSolver,
-    threshold,
+    tol,
     args...,
 )
-    return nothing
+    ΔQ = similar(Q)
+    jvp! = solver.jvp!
+
+    # Compute right-hand side for Jacobian system:
+    # JΔQ = -R
+    # where R = Qrhs - F(Q)
+    R = solver.residual
+    implicitoperator!(R, Q, args...)
+    R .-= Qrhs
+
+    # Solve JΔq = -R
+    linearsolve!(
+        jvp!,
+        solver.linearsolver,
+        ΔQ,
+        R,
+        args...;
+        max_iters = getmaxiterations(solver.linearsolver),
+    )
+
+    # Newton correction
+    Q .+= ΔQ
+
+    # Reevaluate residual with new solution
+    implicitoperator!(R, Q, args...)
+    resnorm = norm(R, weighted_norm)
+    converged = false
+    if resnorm < tol
+        converged = true
+    end
+
+    return converged, resnorm
 end
