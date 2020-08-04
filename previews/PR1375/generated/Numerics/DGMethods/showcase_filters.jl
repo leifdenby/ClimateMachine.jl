691dadf2906564f3f3e583ddb039553dd01c7b6c
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-08-04 05:35:09 +0000

    build based on 7f25a2e

--- previews/PR1375/generated/Numerics/DGMethods/showcase_filters.jl
+++ previews/PR1375/generated/Numerics/DGMethods/showcase_filters.jl
@@ -1,28 +1,38 @@
-ENV["JULIA_LOG_LEVEL"] = "WARN"
+##ENV["JULIA_LOG_LEVEL"] = "WARN"
 using ClimateMachine
 const clima_dir = dirname(dirname(pathof(ClimateMachine)));
 include(joinpath(clima_dir, "tutorials", "Numerics", "DGMethods", "Box1D.jl"))
 
-run_box1D(4, 0.0, 1.0, 1.0, "box_1D_4_no_filter.svg");
+output_dir = @__DIR__;
+mkpath(output_dir);
 
-run_box1D(4, 0.0, 1.0, 1.0, "box_1D_4_tmar.svg", tmar_filter = true);
+run_box1D(4, 0.0, 1.0, 1.0, joinpath(output_dir, "box_1D_4_no_filter.svg"))
 
 run_box1D(
     4,
     0.0,
     1.0,
     1.0,
-    "box_1D_4_cutoff_1.svg",
+    joinpath(output_dir, "box_1D_4_tmar.svg");
+    tmar_filter = true,
+)
+
+run_box1D(
+    4,
+    0.0,
+    1.0,
+    1.0,
+    joinpath(output_dir, "box_1D_4_cutoff_1.svg");
     cutoff_filter = true,
     cutoff_param = 1,
-);
+)
 
 run_box1D(
     4,
     0.0,
     1.0,
     1.0,
-    "box_1D_4_cutoff_3.svg",
+    joinpath(output_dir, "box_1D_4_cutoff_3.svg");
     cutoff_filter = true,
     cutoff_param = 3,
 )
@@ -32,7 +42,7 @@ run_box1D(
     0.0,
     1.0,
     1.0,
-    "box_1D_4_exp_1_4.svg",
+    joinpath(output_dir, "box_1D_4_exp_1_4.svg");
     exp_filter = true,
     exp_param_1 = 1,
     exp_param_2 = 4,
@@ -43,7 +53,7 @@ run_box1D(
     0.0,
     1.0,
     1.0,
-    "box_1D_4_exp_1_8.svg",
+    joinpath(output_dir, "box_1D_4_exp_1_8.svg");
     exp_filter = true,
     exp_param_1 = 1,
     exp_param_2 = 8,
@@ -54,7 +64,7 @@ run_box1D(
     0.0,
     1.0,
     1.0,
-    "box_1D_4_exp_1_32.svg",
+    joinpath(output_dir, "box_1D_4_exp_1_32.svg");
     exp_filter = true,
     exp_param_1 = 1,
     exp_param_2 = 32,
@@ -65,7 +75,7 @@ run_box1D(
     0.0,
     1.0,
     1.0,
-    "box_1D_4_boyd_1_4.svg",
+    joinpath(output_dir, "box_1D_4_boyd_1_4.svg");
     boyd_filter = true,
     boyd_param_1 = 1,
     boyd_param_2 = 4,
@@ -76,7 +86,7 @@ run_box1D(
     0.0,
     1.0,
     1.0,
-    "box_1D_4_boyd_1_8.svg",
+    joinpath(output_dir, "box_1D_4_boyd_1_8.svg");
     boyd_filter = true,
     boyd_param_1 = 1,
     boyd_param_2 = 8,
@@ -87,7 +97,7 @@ run_box1D(
     0.0,
     1.0,
     1.0,
-    "box_1D_4_boyd_1_32.svg",
+    joinpath(output_dir, "box_1D_4_boyd_1_32.svg");
     boyd_filter = true,
     boyd_param_1 = 1,
     boyd_param_2 = 32,
@@ -98,7 +108,7 @@ run_box1D(
     0.0,
     1.0,
     1.0,
-    "box_1D_4_tmar_exp_1_8.svg",
+    joinpath(output_dir, "box_1D_4_tmar_exp_1_8.svg");
     exp_filter = true,
     tmar_filter = true,
     exp_param_1 = 1,
@@ -110,7 +120,7 @@ run_box1D(
     0.0,
     1.0,
     1.0,
-    "box_1D_4_tmar_boyd_1_8.svg",
+    joinpath(output_dir, "box_1D_4_tmar_boyd_1_8.svg");
     boyd_filter = true,
     tmar_filter = true,
     boyd_param_1 = 1,
