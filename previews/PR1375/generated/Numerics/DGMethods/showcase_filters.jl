34b2721b0d68f1d8ae2c6c15fe8c6aa27e324e84
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-08-04 01:48:44 +0000

    build based on ed38ebb

--- previews/PR1375/generated/Numerics/DGMethods/showcase_filters.jl
+++ previews/PR1375/generated/Numerics/DGMethods/showcase_filters.jl
@@ -2,119 +2,73 @@ using ClimateMachine
 const clima_dir = dirname(dirname(pathof(ClimateMachine)));
 include(joinpath(clima_dir, "tutorials", "Numerics", "DGMethods", "Box1D.jl"))
 
-run_box1D(4, 0.0, 1.0, 1.0, "box_1D_4_no_filter.pdf")
+run_box1D(4, 0.0, 1.0, 1.0, "box_1D_4_no_filter.svg");
 
-run_box1D(4, 0.0, 1.0, 1.0, "box_1D_4_tmar.pdf", tmar_filter = true)
+run_box1D(4, 0.0, 1.0, 1.0, "box_1D_4_tmar.svg", tmar_filter = true);
 
 run_box1D(
     4,
     0.0,
     1.0,
     1.0,
-    "box_1D_4_cutoff_1.pdf",
+    "box_1D_4_cutoff_1.svg",
     cutoff_filter = true,
     cutoff_param = 1,
-)
+);
 
-run_box1D(
-    4,
-    0.0,
-    1.0,
-    1.0,
-    "box_1D_4_cutoff_3.pdf",
-    cutoff_filter = true,
-    cutoff_param = 3,
-)
+# `CutoffFilter(grid, Nc=3)`:
+#run_box1D(
 
-run_box1D(
-    4,
-    0.0,
-    1.0,
-    1.0,
-    "box_1D_4_exp_1_4.pdf",
-    exp_filter = true,
-    exp_param_1 = 1,
-    exp_param_2 = 4,
-)
+#)
+# ![](box_1D_4_cutoff_3.svg)
 
-run_box1D(
-    4,
-    0.0,
-    1.0,
-    1.0,
-    "box_1D_4_exp_1_8.pdf",
-    exp_filter = true,
-    exp_param_1 = 1,
-    exp_param_2 = 8,
-)
+# `ExponentialFilter(grid, Nc=1, s=4)`:
+#run_box1D(
 
-run_box1D(
-    4,
-    0.0,
-    1.0,
-    1.0,
-    "box_1D_4_exp_1_32.pdf",
-    exp_filter = true,
-    exp_param_1 = 1,
-    exp_param_2 = 32,
-)
+#)
+# ![](box_1D_4_exp_1_4.svg)
 
-run_box1D(
-    4,
-    0.0,
-    1.0,
-    1.0,
-    "box_1D_4_boyd_1_4.pdf",
-    boyd_filter = true,
-    boyd_param_1 = 1,
-    boyd_param_2 = 4,
-)
+# `ExponentialFilter(grid, Nc=1, s=8)`:
+#run_box1D(
 
-run_box1D(
-    4,
-    0.0,
-    1.0,
-    1.0,
-    "box_1D_4_boyd_1_8.pdf",
-    boyd_filter = true,
-    boyd_param_1 = 1,
-    boyd_param_2 = 8,
-)
+#)
+# ![](box_1D_4_exp_1_8.svg)
 
-run_box1D(
-    4,
-    0.0,
-    1.0,
-    1.0,
-    "box_1D_4_boyd_1_32.pdf",
-    boyd_filter = true,
-    boyd_param_1 = 1,
-    boyd_param_2 = 32,
-)
+# `ExponentialFilter(grid, Nc=1, s=32)`:
+#run_box1D(
 
-run_box1D(
-    4,
-    0.0,
-    1.0,
-    1.0,
-    "box_1D_4_tmar_exp_1_8.pdf",
-    exp_filter = true,
-    tmar_filter = true,
-    exp_param_1 = 1,
-    exp_param_2 = 8,
-)
+#)
+# ![](box_1D_4_exp_1_32.svg)
 
-run_box1D(
-    4,
-    0.0,
-    1.0,
-    1.0,
-    "box_1D_4_tmar_boyd_1_8.pdf",
-    boyd_filter = true,
-    tmar_filter = true,
-    boyd_param_1 = 1,
-    boyd_param_2 = 8,
-)
+# `BoydVandevenFilter(grid, Nc=1, s=4)`:
+#run_box1D(
+
+#)
+# ![](box_1D_4_boyd_1_4.svg)
+
+# `BoydVandevenFilter(grid, Nc=1, s=8)`:
+#run_box1D(
+
+#)
+# ![](box_1D_4_boyd_1_8.svg)
+
+# `BoydVandevenFilter(grid, Nc=1, s=32)`:
+#run_box1D(
+
+#)
+# ![](box_1D_4_boyd_1_32.svg)
+
+# `ExponentialFilter(grid, Nc=1, s=8)` and `TMARFilter()`:
+#run_box1D(
+
+#)
+# ![](box_1D_4_tmar_exp_1_8.svg)
+
+# `BoydVandevenFilter(grid, Nc=1, s=8)` and `TMARFilter()`:
+#run_box1D(
+
+#)
+# ![](box_1D_4_tmar_boyd_1_8.svg)
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
 
