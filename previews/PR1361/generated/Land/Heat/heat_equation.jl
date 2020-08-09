7e8eaf056ba40e658ef6201da027cc3a6d5d38c3
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-08-09 05:31:32 +0000

    build based on 7e116c7

--- previews/PR1361/generated/Land/Heat/heat_equation.jl
+++ previews/PR1361/generated/Land/Heat/heat_equation.jl
@@ -2,6 +2,8 @@ using MPI
 using OrderedCollections
 using Plots
 using StaticArrays
+using OrdinaryDiffEq
+using DiffEqBase
 
 using CLIMAParameters
 struct EarthParameterSet <: AbstractEarthParameterSet end
@@ -35,6 +37,8 @@ import ClimateMachine.BalanceLaws:
     init_state_prognostic!,
     boundary_state!
 
+import ClimateMachine.DGMethods: calculate_dt
+
 FT = Float64;
 
 ClimateMachine.init(; disable_gpu = true);
@@ -43,9 +47,9 @@ const clima_dir = dirname(dirname(pathof(ClimateMachine)));
 
 include(joinpath(clima_dir, "docs", "plothelpers.jl"));
 
-Base.@kwdef struct HeatModel{FT} <: BalanceLaw
+Base.@kwdef struct HeatModel{FT, APS} <: BalanceLaw
     "Parameters"
-    param_set::AbstractParameterSet = param_set
+    param_set::APS
     "Heat capacity"
     ρc::FT = 1
     "Thermal diffusivity"
@@ -58,7 +62,7 @@ Base.@kwdef struct HeatModel{FT} <: BalanceLaw
     flux_top::FT = 0.0
 end
 
-m = HeatModel{FT}();
+m = HeatModel{FT, typeof(param_set)}(; param_set = param_set);
 
 vars_state(::HeatModel, ::Auxiliary, FT) = @vars(z::FT, T::FT);
 
@@ -109,7 +113,6 @@ function update_auxiliary_state!(
     elems::UnitRange,
 )
     nodal_update_auxiliary_state!(heat_eq_nodal_update_aux!, dg, m, Q, t, elems)
-    return true # TODO: remove return true
 end;
 
 function heat_eq_nodal_update_aux!(
@@ -169,9 +172,10 @@ function boundary_state!(
     t,
     _...,
 )
-    if bctype == 1 # bottom
+    # Apply Dirichlet BCs
+    if bctype == 1 # At bottom
         state⁺.ρcT = m.ρc * m.T_bottom
-    elseif bctype == 2 # top
+    elseif bctype == 2 # At top
         nothing
     end
 end;
@@ -190,9 +194,10 @@ function boundary_state!(
     t,
     _...,
 )
-    if bctype == 1 # bottom
-        state⁺.ρcT = m.ρc * m.T_bottom
-    elseif bctype == 2 # top
+    # Apply Neumann BCs
+    if bctype == 1 # At bottom
+        nothing
+    elseif bctype == 2 # At top
         diff⁺.α∇ρcT = n⁻ * m.flux_top
     end
 end;
@@ -216,14 +221,53 @@ driver_config = ClimateMachine.SingleStackConfiguration(
 t0 = FT(0)
 timeend = FT(40)
 
-Δ = min_node_distance(driver_config.grid)
+function calculate_dt(dg, model::HeatModel, Q, Courant_number, t, direction)
+    Δt = one(eltype(Q))
+    CFL = DGMethods.courant(diffusive_courant, dg, model, Q, Δt, t, direction)
+    return Courant_number / CFL
+end
 
-given_Fourier = FT(0.08);
-Fourier_bound = given_Fourier * Δ^2 / m.α;
-dt = Fourier_bound
+function diffusive_courant(
+    m::HeatModel,
+    state::Vars,
+    aux::Vars,
+    diffusive::Vars,
+    Δx,
+    Δt,
+    t,
+    direction,
+)
+    return Δt * m.α / (Δx * Δx)
+end
 
-solver_config =
-    ClimateMachine.SolverConfiguration(t0, timeend, driver_config, ode_dt = dt);
+use_implicit_solver = false
+if use_implicit_solver
+    given_Fourier = FT(30)
+
+    solver_config = ClimateMachine.SolverConfiguration(
+        t0,
+        timeend,
+        driver_config;
+        ode_solver_type = ImplicitSolverType(OrdinaryDiffEq.Kvaerno3(
+            autodiff = false,
+            linsolve = LinSolveGMRES(),
+        )),
+        Courant_number = given_Fourier,
+        CFL_direction = VerticalDirection(),
+    )
+else
+    given_Fourier = FT(0.7)
+
+    solver_config = ClimateMachine.SolverConfiguration(
+        t0,
+        timeend,
+        driver_config;
+        Courant_number = given_Fourier,
+        CFL_direction = VerticalDirection(),
+    )
+end;
+
+
 grid = solver_config.dg.grid;
 Q = solver_config.Q;
 aux = solver_config.dg.state_auxiliary;
@@ -232,57 +276,38 @@ output_dir = @__DIR__;
 
 mkpath(output_dir);
 
-z_scale = 100 # convert from meters to cm
-z_key = "z"
-z_label = "z [cm]"
-z = get_z(grid, z_scale)
-state_vars = SingleStackUtils.get_vars_from_nodal_stack(
-    grid,
-    Q,
-    vars_state(m, Prognostic(), FT),
-)
-aux_vars = SingleStackUtils.get_vars_from_nodal_stack(
-    grid,
-    aux,
-    vars_state(m, Auxiliary(), FT),
-)
-all_vars = OrderedDict(state_vars..., aux_vars...);
-export_plot_snapshot(
+z_scale = 100; # convert from meters to cm
+z_key = "z";
+z_label = "z [cm]";
+z = get_z(grid, z_scale);
+
+all_data = Dict[dict_of_nodal_states(solver_config, [z_key])]  # store initial condition at ``t=0``
+time_data = FT[0]                                      # store time data
+
+export_plot(
     z,
-    all_vars,
+    all_data,
     ("ρcT",),
-    joinpath(output_dir, "initial_condition.png"),
-    z_label,
+    joinpath(output_dir, "initial_condition.png");
+    xlabel = "ρcT",
+    ylabel = z_label,
+    time_data = time_data,
 );
 
 const n_outputs = 5;
 
 const every_x_simulation_time = ceil(Int, timeend / n_outputs);
 
-all_data = Dict[Dict([k => Dict() for k in 0:n_outputs]...),]
-all_data[1] = all_vars # store initial condition at ``t=0``
-
-step = [1];
 callback = GenericCallbacks.EveryXSimulationTime(every_x_simulation_time) do
-    state_vars = SingleStackUtils.get_vars_from_nodal_stack(
-        grid,
-        Q,
-        vars_state(m, Prognostic(), FT),
-    )
-    aux_vars = SingleStackUtils.get_vars_from_nodal_stack(
-        grid,
-        aux,
-        vars_state(m, Auxiliary(), FT);
-        exclude = [z_key],
-    )
-    all_vars = OrderedDict(state_vars..., aux_vars...)
-    push!(all_data, all_vars)
-
-    step[1] += 1
+    push!(all_data, dict_of_nodal_states(solver_config, [z_key]))
+    push!(time_data, gettime(solver_config.solver))
     nothing
 end;
 
 ClimateMachine.invoke!(solver_config; user_callbacks = (callback,));
+
+push!(all_data, dict_of_nodal_states(solver_config, [z_key]));
+push!(time_data, gettime(solver_config.solver));
 
 @show keys(all_data[1])
 
@@ -290,8 +315,10 @@ export_plot(
     z,
     all_data,
     ("ρcT",),
-    joinpath(output_dir, "solution_vs_time.png"),
-    z_label,
+    joinpath(output_dir, "solution_vs_time.png");
+    xlabel = "ρcT",
+    ylabel = z_label,
+    time_data = time_data,
 );
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
